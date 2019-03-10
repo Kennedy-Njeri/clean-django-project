d980916250385d019d2acb1c40a1719e8083f59e
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-10 16:42:11 +0300

    customized the Bookadmin

--- books/admin.py
+++ books/admin.py
@@ -2,5 +2,32 @@ from django.contrib import admin
 from .models import Book, Author
 # Register your models here.
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+
+    """Not a must"""
+    fieldsets = [
+        ("Book Details", {"fields": ["title", "authors"]}),
+
+        ("Review", {"fields": ["is_favourite", "review", "date_reviewed"]})
+    ]
+
+    readonly_fields = ("date_reviewed",)
+
+    def book_authors(self, obj):
+        return obj.list_authors()
+
+    book_authors.short_description = "Author(s)"
+
+    list_display = ("title", "book_authors", "date_reviewed", "is_favourite",)
+
+    list_editable = ("is_favourite",)
+
+    list_display_links = ("title", "date_reviewed",)
+
+    list_filter = ("is_favourite",)
+
+    search_fields = ("title", "authors__name",)
+
+#admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
