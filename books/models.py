7d063a601ddd46ae379348775bb4e58d6d42174f
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-11 23:41:27 +0300

    super method

--- books/models.py
+++ books/models.py
@@ -16,11 +16,7 @@ class Book(models.Model):
     def list_authors(self):
         return ", ".join([author.name for author in self.authors.all()])
 
-    def save(self, *args, **kwargs):
-        if (self.review and self.date_reviewed is None):
-            self.date_reviewed = now()
-
-        super(Book, self).save(*args, **kwargs)
+    
 
 
 
