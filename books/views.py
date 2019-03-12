fd622dfc4848e8ce3f86ee87aae90fef25b5e1e7
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-12 14:59:25 +0300

    created book listings in the template i.e index

--- books/views.py
+++ books/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
 
 
 def list_books(request):
-    return render(request, 'index.html')
+
+    books = Book.objects.exclude(date_reviewed__isnull=True).prefetch_related('authors')
+
+    context = {
+
+        'books': books
+    }
+
+
+    return render(request, 'index.html', context)
