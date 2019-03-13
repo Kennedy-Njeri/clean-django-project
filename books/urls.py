92d135e66b6adff7e5226e1d9714783251c3194c
Kennedy Njeri <mistakenz123@gmail.com>
2019-03-13 23:08:07 +0300

    added review form

--- books/urls.py
+++ books/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('authors/', views.AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', views.BookDetail.as_view(), name="book-detail"),
     url(r'^authors/(?P<pk>[-\w]+)/$', views.AuthorDetail.as_view(), name="author-detail"),
+    url(r'^review/$', views.review_books, name='review-books'),
+    url(r'^review/(?P<pk>[-\w]+)/$', views.review_book, name='review-book'),
 
 
 
