f1f8f2f4b1057652311c3c76ce2c5378ac6f987b
Kennedy Njeri <mistakenz123@gmail.com>
2019-04-05 21:49:55 +0300

    reviewed by model

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     authors = models.ManyToManyField("Author", related_name="books")
     review = models.TextField(blank=True, null=True)
-    #reviewed_by = models.ForeignKey(User, blank=True, null=True, related_name='reviews', on_delete=models.CASCADE)
+    reviewed_by = models.ForeignKey(User, blank=True, null=True, related_name='reviews', on_delete=models.CASCADE)
     date_reviewed = models.DateTimeField(blank=True, null=True)
     is_favourite = models.BooleanField(default=False, verbose_name="Favourite?")
 
